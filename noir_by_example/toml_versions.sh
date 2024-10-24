7c44266403edccebc9337afffeb4855338b41627
James Zaki <james.zaki@proton.me>
2024-10-24 12:05:45 +0100

    Use named param in version script

--- noir_by_example/toml_versions.sh
+++ noir_by_example/toml_versions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 file_prefix="$1"
+echo "Usage: $0 [file prefix]"
+echo "Versions of $file_prefix*.toml files"
 
-find . -iname "$1*.toml" | while read -r file; do
+find . -iname "$file_prefix*.toml" | while read -r file; do
     echo "$file:"  # Print the full path of the file
     grep "version" "$file"
 done
