c1a220b10894f34849b8fcad7ae603645493caa9
saleel <saleel@saleel.xyz>
2025-07-03 12:12:47 +0530

    chore: cleanup next config

--- web-starter/web/nextjs/next.config.mjs
+++ web-starter/web/nextjs/next.config.mjs
@@ -1,62 +1,29 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/verify': [
-        './node_modules/@aztec/bb.js/dest/node/barretenberg_wasm/**/*',
-        './node_modules/@aztec/bb.js/dest/node/barretenberg_wasm/barretenberg_wasm_thread/factory/node/thread.worker.js'
-      ],
-    },
-  },
   webpack: (config) => {
     config.experiments = {
       asyncWebAssembly: true,
-      syncWebAssembly: true,
       layers: true,
     };
     return config;
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       // Exclude all oauth-callback paths
-  //       source: '/((?!oauth-callback).*)',
-  //       headers: [
-  //         {
-  //           key: 'Cross-Origin-Embedder-Policy',
-  //           value: 'require-corp',
-  //         },
-  //         {
-  //           key: 'Cross-Origin-Opener-Policy',
-  //           value: 'same-origin',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       // Special case for iOS devices - disable COOP/COEP
-  //       source: '/((?!oauth-callback).*)',
-  //       headers: [
-  //         {
-  //           key: 'Cross-Origin-Embedder-Policy',
-  //           value: 'unsafe-none',
-  //         },
-  //         {
-  //           key: 'Cross-Origin-Opener-Policy',
-  //           value: 'unsafe-none',
-  //         },
-  //       ],
-  //       // Only apply these headers for iOS devices
-  //       has: [
-  //         {
-  //           type: 'header',
-  //           key: 'user-agent',
-  //           value: '(.*iPhone|iPad|iPod.*)',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
