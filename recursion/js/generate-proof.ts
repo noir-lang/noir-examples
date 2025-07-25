ffba6f25eebf6702b0fedf2c0f6d341ab2b0c7ae
Josh Crites <jc@joshcrites.com>
2025-07-25 11:57:55 -0400

    update

--- recursion/js/generate-proof.ts
+++ recursion/js/generate-proof.ts
@@ -11,7 +11,7 @@ import { CompiledCircuit, Noir } from "@noir-lang/noir_js";
     // Generate proof for inner circuit
     const inputs = { x: 3, y: 3 }
     const { witness } = await innerCircuitNoir.execute(inputs);
-    const { proof: innerProofFields, publicInputs: innerPublicInputs } = await innerBackend.generateProofForRecursiveAggregation(witness);
+    const { proof: innerProofFields, publicInputs: innerPublicInputs } = await innerBackend.generateProof(witness);
 
     // Get verification key for inner circuit as fields
     const innerCircuitVerificationKey = await innerBackend.getVerificationKey();
