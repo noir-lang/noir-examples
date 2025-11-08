07634a19532eeebe61eaddb7557c514f1356693f
Josh Crites <jc@joshcrites.com>
2025-11-07 20:37:28 -0500

    increase timeout

--- web-starter/web/webpack/webpack.config.cjs
+++ web-starter/web/webpack/webpack.config.cjs
@@ -49,7 +49,7 @@ module.exports = {
       directory: path.join(__dirname, 'dist'),
     },
     compress: true,
-    port: 3000,
+    port: 3002,
     hot: true,
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
